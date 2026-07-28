a9f7095a9ac704addca6ec7b5361afad38b674b2
Abdur Rahim <abdur.rahim@cqumail.com>
2026-07-28 16:08:37 +1000

    Initial project setup: Maven skeleton with JUnit 6 and JDK 24 configuration

---
+++ src/main/java/com/cqu/cupid/App.java
@@ -0,0 +1,13 @@
+package com.cqu.cupid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
